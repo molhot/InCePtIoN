84ba2867812418cb998cbf1c520487c98a3430f2
molhot <sakata19991214@gmail.com>
2023-07-02 18:23:26 +0900

    push

--- playground/nginx/config.sh
+++ playground/nginx/config.sh
@@ -1,8 +1,6 @@
 mkdir /etc/nginx/ssl
 openssl genrsa -out /etc/nginx/ssl/server.key 3072
 
-#Create the request (CSR)
 openssl req -new -key /etc/nginx/ssl/server.key -out /etc/nginx/ssl/server.csr -subj "/CN=example.com"
 
-# CRT (SSLサーバー証明書)の作成
 openssl x509 -req -signkey /etc/nginx/ssl/server.key -in /etc/nginx/ssl/server.csr -out /etc/nginx/ssl/server.crt
