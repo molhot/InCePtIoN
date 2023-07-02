e126011fb51f645b9e61d0202a35ece381f0a508
molhot <sakata19991214@gmail.com>
2023-07-02 18:40:58 +0900

    Push

--- playground/nginx/config.sh
+++
@@ -1,6 +0,0 @@
-mkdir /etc/nginx/ssl
-openssl genrsa -out /etc/nginx/ssl/server.key
-
-openssl req -new -key /etc/nginx/ssl/server.key -out /etc/nginx/ssl/server.csr -subj "/CN=example.com"
-
-openssl x509 -req -signkey /etc/nginx/ssl/server.key -in /etc/nginx/ssl/server.csr -out /etc/nginx/ssl/server.crt
